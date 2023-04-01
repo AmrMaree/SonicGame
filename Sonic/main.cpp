124dd2fef6bbf23d21cfa18e80d619f1d4660d2c
AmrMaree <113312326+AmrMaree@users.noreply.github.com>
2023-04-01 17:59:18 +0200

    code change

--- Sonic/main.cpp
+++ Sonic/main.cpp
@@ -5,7 +5,9 @@ using namespace sf;
 using namespace std;
 int main()
 {
-    RenderWindow window(VideoMode(1920, 1080), "Sonic Game");
+    RenderWindow window(sf::VideoMode(1920,1080), "Sonic Game");
+    RectangleShape ground(Vector2f(1920, 10));
+    ground.setPosition(0,800);
 
     // Load the sprite sheet
     Texture sonictexture;
@@ -14,13 +16,15 @@ int main()
     // Create the sprite
     Sprite sonic(sonictexture);
     sonic.setTextureRect(IntRect(0, 0, 102, 105));
-    sonic.setPosition(0, 720);
+    sonic.setPosition(0, 0);
 
     //Variables
     int animeIndicator = 0;
     int coinCount = 0;
     int coinAnimationIndicator = 0;
     bool isCoinVisible = true;
+    bool landed = false;
+    double velocityY = 0;
 
     // Set up coins
     Texture coinsTextures;
@@ -29,7 +33,7 @@ int main()
     coin.setPosition(500, 500);
     coin.setTextureRect(IntRect(0, 0, 134, 134));
     coin.setScale(0.4f, 0.4f);
-
+   
 
     while (window.isOpen())
     {
@@ -45,7 +49,6 @@ int main()
             animeIndicator++;
             sonic.setOrigin(sonic.getLocalBounds().width, 0);
             sonic.setScale(-1, 1);
-
         }
         else if (Keyboard::isKeyPressed(Keyboard::Right))
         {
@@ -54,27 +57,45 @@ int main()
             sonic.setOrigin(0, 0);
             sonic.setScale(1, 1);
         }
-        else if (Keyboard::isKeyPressed(Keyboard::Up))
-        {
-            sonic.move(0, -1);
-        }
-        else if (Keyboard::isKeyPressed(Keyboard::Down))
-        {
-            sonic.move(0, 1);
-        }
-        animeIndicator %= 9;
+            if (sonic.getGlobalBounds().intersects(ground.getGlobalBounds()))
+            {
+                landed = true;
+                velocityY = 0;
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+                    velocityY = sqrtf(9.81f * 0.5f);
+            }
+            else
+            {
+                landed = false;
+                velocityY -= 0.01;
+            }
+             
+            // Check if Sonic is out of bounds
+            if (sonic.getPosition().y < 0.f)
+                sonic.setPosition(sonic.getPosition().x, 0.f);
+            if (sonic.getPosition().x < 0.f)
+                sonic.setPosition(0.f, sonic.getPosition().y);
+
+        animeIndicator %= 10;
         sonic.setTextureRect(IntRect(animeIndicator * 102, 0, 102, 105));
         if (sonic.getGlobalBounds().intersects(coin.getGlobalBounds()))
             isCoinVisible = false;
         coin.setTextureRect(IntRect(coinAnimationIndicator * 134, 0, 134, 134));
         coinAnimationIndicator++;
         coinAnimationIndicator %= 9;
+
+        //2D camera
+        View view(Vector2f(0, 0), Vector2f(1920, 1080));
+        view.setCenter(sonic.getPosition()); //update
+        window.setView(view);
+
         
         // Draw the sprite
         window.clear();
         window.draw(sonic);
+        sonic.move(0, -velocityY);
         if(isCoinVisible)
-        window.draw(coin);
+             window.draw(coin);
         window.display();
     }
 
